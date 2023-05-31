a3a6599ffbec4ea8c3aed7216eb8b80dd84d6652
nandaaolvra <deoliveiraborgesfernanda@gmail.com>
2023-05-31 14:55:28 -0300

    Projeto Lanchonete

--- backend/app/models/produto.model.js
+++ backend/app/models/produto.model.js
@@ -1,14 +1,38 @@
 const sql = require("./db.js");
 //construtor
 const ProdutoModel = function(produto){
-    this.name = produto.nome;
+    this.nome = produto.nome;
     this.valor = produto.valor;
 }
 //cria novo prodtuo no banco
 ProdutoModel.create = (produto, result) => {
+    sql.query("insert into produtos set ?", produto, (err, res) => {
+        if(err){
+            console.log("Erro: ", err);
+        result(err, null);
+        return;        
+    }
+
+    console.log("Produto criado: ", {idprodutos: res.insertId, ...produto});
+    result(null, {idprodutos: res.insertId, ...produto});
+    })
 };
 //seleciona produto por ID
-ProdutoModel.findById = (produto, result) => {
+ProdutoModel.findById = (produtoId, result) => {
+    sql.query("Select * from produtos where idprodutos = "+produtoId, (err, res) =>{
+        if (err) {
+            console.log("erro: ", err);
+            result(null, err);
+            return;
+        }
+        if(res.length) {
+            console.log("Produto Encontrado", res[0]);
+            result(null,res[0]);
+        } else {
+            result({type: "not_found"}, null);
+            console.log("Produto não encontrado");
+        }
+    })
 };
 //seleciona todos os produtos
 ProdutoModel.getAll = result => {
@@ -25,6 +49,18 @@ ProdutoModel.getAll = result => {
 };
 //atualizar produto por id
 ProdutoModel.updateById = (produtoId, produto, result) => {
+    sql.query("UPDATE produtos SET nome = ?, valor = ? WHERE idprodutos = ?",
+                 [produto.nome, produto.valor, produtoId], (err, res) => {
+                    if (err){
+                    console.log("erro: ", err);
+                    result(null, err);
+                    } else if (res.affectedRows == 0){
+                    result ({ type: "not_fun"}, null);
+                    } else {
+                    console.log("Produto atualizado: ", {idprodutos: produtoId, ...produto});
+                    result(null, {idprodutos: produtoId, ...produto});
+                }
+           });
 };
 // remover produto por id 
 ProdutoModel.remove = (produtoId, result) => {
